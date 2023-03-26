4b5a119d85f6f1cb9e2a87f6e22b67abc35eb4f6
LoanNguyenThi5 <Loan.NguyenThi5@nashtechglobal.com>
2023-03-26 16:08:23 +0700

    commit base

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,23 @@
-package org.example;public class Main {
+package org.example;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Main {
+    public static final String PROPERTY_CHROME_DRIVER = "webdriver.chrome.driver";
+
+    public static final String PROPERTY_CHROME_CLIENT = "webdriver.chrome.client";
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.setProperty(PROPERTY_CHROME_DRIVER, "/Users/loannguyen/Documents/Member/loan/Driver/chromedriver/chromedriver"); //XXX Configure me!
+       // System.setProperty(PROPERTY_CHROME_CLIENT, "C:\\DevTools\\Driver\\chrome72-selenium\\chrome-DLee\\chrome.exe"); //XXX Configure me!
+
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://the-internet.herokuapp.com/");
+        driver.findElement(By.linkText("Form Authentication")).click();
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+        driver.quit();
     }
 }
